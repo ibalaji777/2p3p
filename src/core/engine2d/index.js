666a05b8222c1a670070f5ac0825c3fc8a982532
ibalaji777 <balajiofficialmailme@gmail.com>
2026-04-09 07:50:33 +0530

    railing and wall

--- src/core/engine2d/index.js
+++ src/core/engine2d/index.js
@@ -676,7 +676,7 @@ export class FloorPlanner {
     exportState() {
         const state = {
             walls: this.walls.map(w => ({
-                startX: w.startAnchor.x, startY: w.startAnchor.y, endX: w.endAnchor.x, endY: w.endAnchor.y, thickness: w.thickness || w.config.thickness, height: w.height || w.config?.height || 120, type: w.type, configId: w.configId,
+                startX: w.startAnchor.x, startY: w.startAnchor.y, endX: w.endAnchor.x, endY: w.endAnchor.y, thickness: w.thickness || w.config.thickness, height: w.height !== undefined ? w.height : (w.config?.height || 120), type: w.type, configId: w.configId,
                 pts: w.poly ? w.poly.points() : null,
                 widgets: w.attachedWidgets.map(wid => ({ t: wid.t, configId: wid.type, width: wid.width })),
                 decors: w.attachedDecor ? w.attachedDecor.map(d => ({ id: d.id, configId: d.configId, side: d.side, localX: d.localX, localY: d.localY, localZ: d.localZ, width: d.width, height: d.height, depth: d.depth, tileSize: d.tileSize, faces: { front: d.faces.front, back: d.faces.back, left: d.faces.left, right: d.faces.right } })) : []
